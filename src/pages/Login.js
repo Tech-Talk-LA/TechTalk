dc51191264f71b50e09ec34a6eaa2056dda8af33
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-07-22 22:06:37 -0700

    set up object for login js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,9 +5,37 @@ import { createStackNavigator } from '@react-navigation/stack';
 import styles from "../../assets/styles.js";
 //import UserFeed from "./UserFeed.js";
 
+
+
 export default function Login({ navigation }) {
-    const [userNameInput, setUserNameInput] = React.useState('UserName')
-    const [passWordInput, setPassWordInput] = React.useState('Password')
+    const [userNameInput, setUserNameInput] = React.useState('')
+    const [passWordInput, setPassWordInput] = React.useState('')
+
+    function loginValidation() {
+        if (userNameInput === '') {
+            alert('Please enter a username')
+            return false
+        }
+        if (passWordInput === '') {
+            alert('Please enter your password')
+            return false
+        }
+        return true
+    }
+
+    function apiCall() {
+        if (loginValidation()) {
+            alert('Login Successful')
+            console.log(loginData)
+        }
+    }
+
+    const loginData = {
+        username: userNameInput,
+        password: passWordInput,
+    }
+
+
     return (
         <View style={styles.container}>
             <Text name="title">Welcome to Tech Talk</Text>
@@ -16,12 +44,14 @@ export default function Login({ navigation }) {
                 style={styles.textInput}
                 onChangeText={(text) => setUserNameInput(text)}
                 value={userNameInput}
+                placeholder="username"
             />
             <Text>Password</Text>
             <TextInput
                 style={styles.textInput}
                 onChangeText={(text) => setPassWordInput(text)}
                 value={passWordInput}
+                placeholder="password"
                 secureTextEntry={true}
             />
             <TouchableOpacity>
@@ -32,6 +62,7 @@ export default function Login({ navigation }) {
                         //     ? navigation.navigate("UserFeed")
                         //     : navigation.navigate("SignUp");
                         navigation.navigate("User Feed")
+                        console.log(loginData)
                     }}
                 >
                     LogIn
